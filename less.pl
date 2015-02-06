fa3d898ee2f3a63933490f97082c3406ce41a58d
Jay Hannah <jay.hannah@iinteractive.com>
2015-02-05 19:58:51 -0600

    comments and such

--- less.pl
+++ less.pl
@@ -1,7 +1,9 @@
 use v5.20;
 use HTML::TableExtract qw(tree);
-use Data::Printer;
+#use Data::Printer;
 
+
+# Read the file which tells us which sites we care about.
 my %sites;
 open my $in, "<", "sites.txt";
 while (<$in>) {
@@ -10,10 +12,14 @@ while (<$in>) {
 }
 close $in;
 
+
+# Parse the input HTML
 my $te = HTML::TableExtract->new();
 $te->parse_file("input/in.html");
 my ($top_table, $bottom_table) = $te->tables;
 
+
+# Nuke the <td>s we don't want out of the top table.
 foreach my $row ($top_table->rows) {
   foreach my $f (@$row) {
     if (ref $f eq 'HTML::ElementTable::DataElement') {
@@ -25,6 +31,8 @@ foreach my $row ($top_table->rows) {
   }
 }
 
+
+# Nuke the <tr>s we don't want out of the bottom table.
 my @rows_to_delete;
 my $row_count = 0;
 foreach my $row ($bottom_table->rows) {
@@ -37,15 +45,15 @@ foreach my $row ($bottom_table->rows) {
   }
   $row_count++;
 }
-
 my $tree = $bottom_table->tree;
 foreach my $i (reverse @rows_to_delete) {
   my ($p) = $tree->row($i)->parent;
   $p->delete;
 }
 
+
+# Print the whole page:
 $tree = $te->tree;
 say $tree->as_HTML;
-
 
 
