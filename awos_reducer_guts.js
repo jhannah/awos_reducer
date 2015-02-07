a7e0e83e150a44952afef7dbdd1bbac3022d11d8
Jay Hannah <jay.hannah@iinteractive.com>
2015-02-07 15:10:51 -0600

    more

--- awos_reducer_guts.js
+++ awos_reducer_guts.js
@@ -1,18 +1,25 @@
-
+// http://code.tutsplus.com/tutorials/create-bookmarklets-the-right-way--net-18154
 if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
-    script = document.createElement( 'script' );
-   script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'; 
-    script.onload=releasetheKraken;
-    document.body.appendChild(script);
+  script = document.createElement( 'script' );
+  script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'; 
+  script.onload=releasetheKraken;
+  document.body.appendChild(script);
 } 
 else {
-    releasetheKraken();
+  releasetheKraken();
 }
  
 function releasetheKraken() {
   // The Kraken has been released, master!
   // Yes, I'm being childish. Place your code here 
-  alert("krak j rules!");
+  alert("kr j rules!");
+  $("td").toggle(function(){
+    $(this).css('background-color','blue')
+  },function(){
+    $(this).css('background-color','green')
+  });
+
+
 }
 
 
