8a119a10219c58b6dd003aaed42e451765e2e827
Jay Hannah <jay.hannah@iinteractive.com>
2015-02-07 14:21:34 -0600

    raw

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 See less AWOS
 
+[drag this to install](https://raw.githubusercontent.com/jhannah/awos_reducer/master/awos_reducer.js)
 
--- awos_reducer.js
+++ awos_reducer.js
@@ -1,6 +1,6 @@
 javascript: (function () { 
   var jsCode = document.createElement('script'); 
-  jsCode.setAttribute('src', 'https://github.com/jhannah/awos_reducer/blob/master/awos_reducer_guts.js');                  
+  jsCode.setAttribute('src', 'https://raw.githubusercontent.com/jhannah/awos_reducer/master/awos_reducer_guts.js');
   document.body.appendChild(jsCode); 
 }());
 
