b859838cbbea5fe049dad86868c93463ef4ca71b
Jay Hannah <jay.hannah@iinteractive.com>
2017-05-02 14:32:52 -0500

    Ha, oops, the actual file

---
+++ jenkins_tap_reducer_guts.js
@@ -0,0 +1,18 @@
+// http://code.tutsplus.com/tutorials/create-bookmarklets-the-right-way--net-18154
+if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too
+  script = document.createElement( 'script' );
+  script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'; 
+  script.onload=releasetheKraken;
+  document.body.appendChild(script);
+} 
+else {
+  releasetheKraken();
+}
+ 
+function releasetheKraken() {
+  // The Kraken has been released, master!
+  // Yes, I'm being childish. Place your code here 
+  $("table.tap tbody td.green").parents('tr').css("display", "none");
+}
+
+
